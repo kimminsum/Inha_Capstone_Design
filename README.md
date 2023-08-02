d05c2456a8e3eb3b80329d0d6cf4c9e56fdb2b8a
kimminsum <tommy1005a@gmail.com>
2023-08-02 13:52:45 +0900

    complete mission

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # futaritojokinshi
 
+git push https://github.com/kimminsum/futaritojokinshi.git
+
 ### This project's subject is "No double boarding".
+
 ## [Road Map]
+
 ### First of all, we try to connect to pressure sensor.
+
 ### Second, we need to make more 1 analog port I think it would be solved by awm port maybe?
+
 ### Third, we just coding about how to forbid double boarding.
--- futari/futari.ino
+++ futari/futari.ino
@@ -30,7 +30,14 @@ const double R_DIV = 3230.0; // Measured resistance of 3.3k resistor
 
 /* Sensor Pin Setting */
 const int pin_length = 6;
-const int pin_arr[pin_length] = {A0, A1, A2, A3, A4, A5};
+const int pin_arr[pin_length] {A0, A1, A2, A3, A4, A5};
+
+
+/* Target Sensors */
+// fst
+const int fst_target_arr[pin_length/2] {0, 1, 2};
+// snd
+const int snd_target_arr[pin_length/2] {3, 4, 5};
 
 
 /* Delay time */
@@ -40,7 +47,7 @@ const int delay_time = 500;
 /* Function Prototype */
 void pin_setup(const int, const int&); // Set up initial pin settings
 void print_db(const double); // Show information about pressure sensors
-const bool over_two(const double); // Judge over two guys riding on kickboard
+const int over_two(const double); // Judge over two guys riding on kickboard
 
 
 
@@ -61,7 +68,7 @@ void loop()
   // Read db from analogPins
   int ADC_arr[pin_length];
   int sum{0};
-  for (int i = 0; i < pin_length; i++)
+  for (int i = 0; i < pin_length; ++i)
   {
     int tmp = analogRead(pin_arr[i]);
     ADC_arr[i] = tmp;
@@ -75,15 +82,16 @@ void loop()
   {
     double voltage_arr[pin_length];
     // Use ADC reading to calculate voltage:
-    for (int i = 0; i < pin_length; i++)
+    for (int i = 0; i < pin_length; ++i)
       voltage_arr[i] = ADC_arr[i] * VCC / 1023.0;
 
     // Use voltage and static resistor value to calculate FSR resistance:
     double resistance_arr[pin_length];
-    for (int i = 0; i < pin_length; i++)
+    for (int i = 0; i < pin_length; ++i)
       resistance_arr[i] = R_DIV * (VCC / voltage_arr[i] - 1.0);
 
-    print_db(resistance_arr);
+    // print_db(resistance_arr);
+    Serial.println(over_two(resistance_arr, fst_target_arr, snd_target_arr));
   }
 }
 
@@ -97,7 +105,7 @@ analog pin: 6, digital pin: 2
 void pin_setup(const int pin_arr[], const int& pin_length)
 {
   // Turn on pressure sensors
-  for (int i = 0; i < pin_length; i++)
+  for (int i = 0; i < pin_length; ++i)
     pinMode(pin_arr[i], INPUT);
 }
 
@@ -106,7 +114,7 @@ void pin_setup(const int pin_arr[], const int& pin_length)
 void print_db(const double resistance_arr[])
 {
   Serial.println("===========================");
-  for (int i = 0; i < pin_length; i++)
+  for (int i = 0; i < pin_length; ++i)
     Serial.println("Resistance" + String(i) + ": " + String(resistance_arr[i]) + " ohms");
   delay(delay_time);
 }
@@ -117,7 +125,28 @@ Judge how many guys ride kickboard
 If over two --> true
 not --> false
 */
-const bool over_two(const double resistance_arr[])
+const int over_two(const double resistance_arr[], const int fst_target_arr[], const int snd_target_arr[])
 {
+  Serial.println("===========================");
 
+  int fst_cnt{1};
+  int snd_cnt{1};
+  for (int i = 0; i < pin_length/2; ++i)
+  {
+    // User should step on foot-shaped stickers
+    fst_cnt &= activation_func(resistance_arr[fst_target_arr[i]]);
+    snd_cnt &= activation_func(resistance_arr[snd_target_arr[i]]);
+  }
+  delay(delay_time);
+  Serial.println((fst_cnt || snd_cnt) ? !(fst_cnt && snd_cnt) : 0);
+  return (fst_cnt || snd_cnt) ? !(fst_cnt && snd_cnt) : 0;
+}
+
+/* Activation Fuction */
+inline const int activation_func(const double& target)
+{
+  if (target >= 3000.0)
+    return 1;
+  else
+    return 0;
 }
